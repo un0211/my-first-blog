432b3008da7fc984b90bb8a3bce8a0305dad2af5
un0211 <un0211@kaist.ac.kr>
2017-05-13 19:17:49 +0900

    first share: logo image

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,10 @@ from .models import Post
 from .forms import PostForm
 
 # Create your views here.
+def main(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts':posts}) #request와 템플릿을 받아 내용이 리턴 브러우저로
+
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts}) #request와 템플릿을 받아 내용이 리턴 브러우저로
